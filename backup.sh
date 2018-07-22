33ba984159c260ab90cdbf9e54f553079413fd87
mildamade <milda.dvorak@gmail.com>
2018-07-23 00:04:09 +0200

    oprava backup, nový na git

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@ export PGPASSWORD="odoo"
 docker exec -i odooalcadocker_odoo11_1 /bin/bash -c \
 "export PGPASSWORD="odoo" \
 && /usr/bin/pg_dump --host=c86e898b85ef --dbname=ALCA --username=odoo" \
-> ./alca_postgresql.dump
+> /data/odoo_backup_db/alca_postgresql.dump
